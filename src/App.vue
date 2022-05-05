25a3bb3a48646abbde51b35f6dc390d55e1ca561
Atiipik <73227837+Atiipik@users.noreply.github.com>
2022-05-05 09:49:19 +0200

    Style : Home page

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,32 @@ export default defineComponent({
   }
 });
 </script>
+
+<style>
+@font-face {
+    font-family: 'Kinetic';
+    src: url('../public/assets/fonts/KineticBlack.eot');
+    src: url('../public/assets/fonts/KineticBlack.eot?#iefix') format('embedded-opentype'),
+        url('../public/assets/fonts/KineticBlack.woff2') format('woff2'),
+        url('../public/assets/fonts/KineticBlack.woff') format('woff'),
+        url('../public/assets/fonts/KineticBlack.ttf') format('truetype'),
+        url('../public/assets/fonts/KineticBlack.svg#KineticBlack') format('svg');
+    font-weight: 900;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Kinetic';
+    src: url('../public/assets/fonts/KineticBold.eot');
+    src: url('../public/assets/fonts/KineticBold.eot?#iefix') format('embedded-opentype'),
+        url('../public/assets/fonts/KineticBold.woff2') format('woff2'),
+        url('../public/assets/fonts/KineticBold.woff') format('woff'),
+        url('../public/assets/fonts/KineticBold.ttf') format('truetype'),
+        url('../public/assets/fonts/KineticBold.svg#KineticBold') format('svg');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+}
+
+</style>
